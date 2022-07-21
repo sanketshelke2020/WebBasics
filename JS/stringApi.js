664c4906b64d9a19cabe21c97f5d0f8ff5971c32
Sanket Shelke <sanketshelke1317@gmail.com>
2022-07-21 18:04:32 +0530

    func1

---
+++ JS/stringApi.js
@@ -0,0 +1,15 @@
+var s1 = "hello"
+var s2 = new String("Hello")
+
+console.log(s1.length);
+let ch = s1.charAt(3);
+console.log(ch)
+console.log(s1.indexOf('ll'));
+
+s1 = s1.concat(" Hello")
+console.log(s1)
+
+var s3= "HellO  "
+var s1 = "Hello   "
+console.log(s1.trim().toUpperCase() === s3.trim().toUpperCase());
+
